bc14acd2fbce27db34ea2fd7a62c4e6fc35adb84
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2013-02-04 00:49:37 +0900

    Move user function

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -31,65 +31,13 @@ class PagesController extends AppController {
 	 *
 	 */
 	public function login() {
-		$redirect = $this->request->query["redirect"];
+		$redirect = ($this->request->query["redirect"]) ?: "";
 		$params = array(
-				"scope" => "create_event",
-				"redirect_uri" => Router::url("/pages/fb_auth/".$redirect, true)
+				"scope" => "create_event,email",
+				"redirect_uri" => Router::url("/users/fb_auth/".$redirect, true)
 			);
 		$url = $this->facebook->getLoginUrl($params);
 		$this->set("fb_login", $url);
-	}
-		
-	/**
-	 * ログアウト
-	 *
-	 */
-	public function logout() {
-		$this->Session->destroy();
-		$this->redirect("/");
-	}
-	
-	public function fb_auth($redirect = "") {
-		$user = $this->facebook->getUser();
-		if ($user) {
-			try {
-				$user_profile = $this->facebook->api('/me');
-//				log_message('error',print_r($user_profile, true));
-				if (isset($user_profile["id"])) {
-//					$user_info = $this->User_model->oauth_login("facebook", $user_profile);
-					$user_info = $result = array(
-							"id"			=> 1,
-							"login_id"		=> "kiyomizu",
-							"facebook_id"	=> "1234567890",
-							"name"			=> "清水",
-							"email"			=> "hiroyuki.kiyomizu@gmail.com"
-					);
-					
-					$this->Session->write("user_info", $user_info);
-					switch ($redirect) {
-						case "tour":
-							$this->redirect("/tours/form");
-							break;
-							
-						case "spot":
-							$this->redirect("/spots/form");
-							break;
-							
-						case "mypage":
-							$this->redirect("/users/");
-							break;
-							
-						default:
-							$this->redirect("/");
-					}
-				}
-			} catch (FacebookApiException $e) {
-				$user = null;
-			}
-		} else {
-			// ログイン画面
-			//$this->redirect("login_form");
-		}
 	}
 	
 	/**
