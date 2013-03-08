09155e37503dd1c8aa8722bdd3098b7656cd28d0
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2013-03-08 17:03:41 +0900

    facebook like button subscribe

--- app/webroot/js/apps/common.js
+++ app/webroot/js/apps/common.js
@@ -553,59 +553,7 @@ $.extend({
 	}
 });
 
-var FB = null;
 var google = null;
-$(function() {
-	if (!FB) {
-		return false;
-	}
-	// like
-	FB.Event.subscribe('edge.create', function(response) {
-		var url = $.url(response);
-		var path_info = url.attr("path").split("/");
-		var fb_auth = FB.getAuthResponse();
-		$.ajax({
-			url: gBaseUrl + "user/spot/like_plus",
-			async: false,
-			data: {
-				user_id: fb_auth.userID,
-				spot_id: path_info[path_info.length -1]
-			},
-			dataType: "json",
-			type: "post",
-			success: function(json) {
-				console.log(json);
-			},
-			error: function() {
-				console.log(arguments);
-			}
-		});
-	});
-	// not like
-	FB.Event.subscribe('edge.remove', function(response) {
-		var url = $.url(response);
-		var path_info = url.attr("path").split("/");
-		var fb_auth = FB.getAuthResponse();
-		// ソート順用
-		$.ajax({
-			url: gBaseUrl + "user/spot/like_minus",
-			async: false,
-			data: {
-				user_id: fb_auth.userID,
-				spot_id: path_info[path_info.length -1]
-			},
-			dataType: "json",
-			type: "post",
-			success: function(json) {
-				console.log(json);
-			},
-			error: function() {
-				console.log(arguments);
-			}
-		});
-	});
-});
-
 (function($) {
 	//pluginNameは任意のプラグイン名に$(elm).pluginName()と使います。
 	$.fn.simplePaginate = function(config) {
