58e2c40af0316b4f67c2e38070de834d726306cc
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2013-04-22 19:14:34 +0900

    app/Model/Behavior/TabitsukuBehavior.php

---
+++ app/Model/Behavior/TabitsukuBehavior.php
@@ -0,0 +1,9 @@
+<?php
+App::import('Model','Category');
+App::import('Model','Tag');
+App::import('Model','Tour');
+App::import('Model','Spot');
+
+class TabitsukuBehavior extends ModelBehavior {
+
+}
